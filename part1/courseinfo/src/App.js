d15ff8f53b71bdaf307a82f7b6f1c57bd9cea3f0
vikss <viraktijain01@gmail.com>
2022-10-25 23:42:41 +0530

    Modified the courseinfo exercise

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,46 +1,55 @@
+import Course  from "./Course"
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development', parts: [
-      { name: 'Fundamentals of React', exercises: 10 },
-      { name: 'Using props to pass data', exercises: 7 },
-      { name: 'State of a component', exercises: 14 }]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
 
+  return <Course courses={courses}></Course>
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course}></Content>
-      <Total course={course}></Total>
-    </div>
-  )
 }
 
-const Header = ({course}) => {
-
-
-  return (<h1>{course.name}</h1>)
-
-}
-const Content = (props) => {
-  let parts = props.course.parts;
-  return (<div><Part part={parts[0].name} exercise={parts[0].exercises}></Part>
-    <Part part={parts[1].name} exercise={parts[1].exercises}></Part>
-    <Part part={parts[2].name} exercise={parts[2].exercises}></Part></div>)
-
-}
-const Part = (props) => {
-  return (<p>{props.part} {props.exercise}</p>)
-}
-const Total = ({ course}) => {
-  let total = 0;
-  for (let i = 0; i < course.parts.length; i++) {
-    total += course.parts[i].exercises;
-
-  }
-  return (<p>Number of exercises {total}</p>)
-
-}
 
 export default App
