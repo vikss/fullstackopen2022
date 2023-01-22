ff44ff526ff513cab2a263a60ca8c9e506ea4487
vikss <viraktijain01@gmail.com>
2023-01-22 22:12:25 +0530

    Lint-ed

--- part3/model/person.js
+++ part3/model/person.js
@@ -1,16 +1,16 @@
-const mongo = require('mongoose')
-mongo.set('strictQuery', true)
-require('dotenv').config()
+const mongo = require("mongoose")
+mongo.set("strictQuery", true)
+require("dotenv").config()
 
 const url = process.env.MONGODB_URI
 const entrySchema = new mongo.Schema({
-    name: { type: String, required: true, minLength: 3 }, number: { type: String, validate: {validator: function(v){
-       
+    name: { type: String, required: true, minLength: 3 }, number: { type: String, validate: { validator: function(v){
 
-        return /(\d{2,3}-\d{5,})|(\d{8,})/.test(v);
-    }, message: props => `${props.value} is not a valid phone number!`},required: true, minLength: 8 }
+
+        return /(\d{2,3}-\d{5,})|(\d{8,})/.test(v)
+    }, message: props => `${props.value} is not a valid phone number!` },required: true, minLength: 8 }
 })
-entrySchema.set('toJSON', {
+entrySchema.set("toJSON", {
 
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
@@ -19,12 +19,12 @@ entrySchema.set('toJSON', {
     }
 
 })
-const Person = mongo.model('Person', entrySchema)
+const Person = mongo.model("Person", entrySchema)
 
 
 mongo.connect(url).then((result) => {
 
-    console.log('Connected to the DB.')
+    console.log("Connected to the DB.")
 
 }).catch((err) => console.log(`Connection to the DB failed with error - ${err}`))
 
